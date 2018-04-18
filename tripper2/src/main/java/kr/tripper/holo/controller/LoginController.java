a8646d4e87b287a43dee3bd7c1f7e0bec707aef7
Administrator <Administrator@WIN7-20110816YW>
2018-04-18 14:47:41 +0900

    로그인

--- tripper2/src/main/java/kr/tripper/holo/controller/LoginController.java
+++ tripper2/src/main/java/kr/tripper/holo/controller/LoginController.java
@@ -1,18 +1,21 @@
-package kr.tripper.holo.login;
+package kr.tripper.holo.controller;
 
 
 import java.util.Locale;
+
+import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.tripper.holo.dao.MemberDAOImpl;
-import kr.tripper.holo.dto.MemberVO;
+import kr.tripper.holo.service.LoginService;
 
 /**
  * Handles requests for the application home page.
@@ -22,8 +25,8 @@ public class LoginController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	@Autowired
-	MemberDAOImpl mDaoImpl;
+	@Resource(name="loginService")
+	private LoginService loginService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String mainlogin(Locale locale, Model model) {
@@ -32,9 +35,7 @@ public class LoginController {
 	
 	@RequestMapping(value = "/signin", method = RequestMethod.POST)
 	public String signin(String email,String password) {
-		System.out.println(email);
-		System.out.println(password);
-		String move = mDaoImpl.signin(email,password);
+		String move = loginService.signin(email,password);
 		return move;
 	}
 	
--- tripper2/src/main/java/kr/tripper/holo/dao/MemberDAO.java
+++ tripper2/src/main/java/kr/tripper/holo/dao/MemberDAO.java
@@ -3,7 +3,7 @@ package kr.tripper.holo.dao;
 import kr.tripper.holo.dto.MemberVO;
 
 public interface MemberDAO {
-	String signin(String email, String pwd);
+	String signin(String email);
 	boolean rememberid();
 	void signup(MemberVO mVo);
 }
